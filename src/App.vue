1267f079525fedd2adf7f2c25dfbf5a1713980e2
Cima9 <97455188+Cima9@users.noreply.github.com>
2022-01-19 09:59:09 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <NavBar />
+</template>
+
+<script>
+import "./assets/CSS/MyCSS.css";
+import NavBar from "./components/NavBar.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/src/jquery.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+.sidebarToggle {
+  font-size: 16px;
+  background-color: #c5c6c7;
+  color: rgb(0, 0, 0, 0.8);
+  margin-left: -19px;
+  z-index: 999;
+  border-radius: 50px;
+  outline: none !important;
+  box-shadow: none !important;
+  transition: all 0.8s ease;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card" style="width: 300px">
+    <img
+      class="card-img-top"
+      src="../assets/LogoCards.jpg"
+      alt="Card image"
+      style="width: 100%"
+    />
+    <div class="card-body bg-primary text-light" style="border-radius: 5px">
+      <h4 class="card-title">{{ nomeCompleto }}</h4>
+      <p class="card-text">{{ nomeCompleto }} chiede di essere assunto :D</p>
+      <a :href="link" class="btn btn-dark" target="_blank">See Git Profile</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: {
+    nomeCompleto: String,
+    link: String,
+  },
+};
+</script>
